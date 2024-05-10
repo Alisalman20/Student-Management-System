5a48fe3159945cdca652ae7628bb711b779c3a88
Moosa Raza <razasyedmoosa882@gmail.com>
2024-05-10 12:40:53 +0500

    Update main.cpp

--- Cpp Files/main.cpp
+++ Cpp Files/main.cpp
@@ -1,1 +1,18 @@
 
+#include"Login.h"
+#include"SignIn.h"
+#include"AdminStudent.h"
+#include"Admin.h"
+#include"AdminCourse.h"
+using namespace StudentManagementSystem;
+using namespace System;
+using namespace System::Windows::Forms;
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+		StudentManagementSystem::AdminCourse loginForm;
+		loginForm.ShowDialog();
+}
+
